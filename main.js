c5fedb63c361eaf19b732b143959aea3af988bc7
mehmetsekeroglu <mehmetsekeroglu2019@gmail.com@>
2020-11-21 19:17:40 +0100

    alert added in main.js

--- main.js
+++ main.js
@@ -1,1 +1,12 @@
 //JS
+//console.log("merhaba");
+//alert("heyy")
+
+var tahminTekrarSayisi = 0;
+let tahminSayisi = 1;
+const toplamHakSayisi = 3;
+
+const oyuncuIsmi = "HiCoders";
+
+console.log(tahminTekrarSayisi);
+console.log(toplamHakSayisi)
